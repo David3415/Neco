995716adfee32671cf3d82b2ccb3bd76b494149c
David <Dgagiev@gmail.com>
2024-05-17 15:34:14 +0300

    RecycleView Plants app finished

--- app/src/main/java/com/example/neco1/MainActivity.kt
+++ app/src/main/java/com/example/neco1/MainActivity.kt
@@ -4,18 +4,40 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.neco1.constance.Constance
 import com.example.neco1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var bindingClass: ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
+    private val adapter = PlantAdapter()
+    private val imageList = listOf(
+        R.drawable.plant1,
+        R.drawable.plant2,
+        R.drawable.plant3,
+        R.drawable.plant4,
+        R.drawable.plant5
+    )
+    private var index = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+    }
 
-        setContentView(bindingClass.root)
-
-
+    private fun init() {
+        binding.apply {
+            rcView.layoutManager = GridLayoutManager(this@MainActivity, 3)
+            rcView.adapter = adapter
+            buttonAdd.setOnClickListener {
+                if (index > 4) index = 0
+                var plant = Plant(imageList[index], "Plant $index")
+                adapter.addPlant(plant)
+                index++
+            }
+        }
     }
 }
 
