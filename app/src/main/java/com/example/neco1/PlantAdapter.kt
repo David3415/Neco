928c19b4e5a62e24c18e75f392aed4234423ac14
David <Dgagiev@gmail.com>
2024-05-17 13:29:16 +0300

    RecycleView first

---
+++ app/src/main/java/com/example/neco1/PlantAdapter.kt
@@ -0,0 +1,12 @@
+package com.example.neco1
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class PlantAdapter:RecyclerView.Adapter<PlantAdapter.PlantHolder> {
+    class PlantHolder(item:View):RecyclerView.ViewHolder (item){
+fun bind(plant: Plant){
+
+}
+    }
+}
