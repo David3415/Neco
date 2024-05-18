343b5b9782cbea3957934b0e37d9bf5df639f341
David <Dgagiev@gmail.com>
2024-05-18 09:35:26 +0300

    RecycleView part2 canvas encreased

---
+++ app/src/main/java/com/example/neco1/EditActivity.kt
@@ -0,0 +1,19 @@
+package com.example.neco1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.neco1.databinding.ActivityMainBinding
+
+class EditActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
