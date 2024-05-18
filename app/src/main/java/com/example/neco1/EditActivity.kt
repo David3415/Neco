a07cbc244bd68924b2e2c7eed164c265d4f59a32
David <Dgagiev@gmail.com>
2024-05-18 17:18:57 +0300

    RecycleView part2 done

--- app/src/main/java/com/example/neco1/EditActivity.kt
+++ app/src/main/java/com/example/neco1/EditActivity.kt
@@ -1,5 +1,6 @@
 package com.example.neco1
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
@@ -10,7 +11,7 @@ import com.example.neco1.databinding.ActivityMainBinding
 class EditActivity : AppCompatActivity() {
     lateinit var binding: ActivityEditBinding
     private var indexImage = 0
-    private var imageId = R.drawable.plant1
+    private var imageId: Int = R.drawable.plant1
     private val imageList = listOf(
         R.drawable.plant1,
         R.drawable.plant2,
@@ -24,16 +25,25 @@ class EditActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityEditBinding.inflate(layoutInflater)
         setContentView(binding.root)
-initButtons()
+        initButtons()
     }
 
     private fun initButtons() = with(binding) {
         bNext.setOnClickListener {
             indexImage++
-            if (indexImage > imageList.size-1) indexImage = 0
-            imageId=imageList[indexImage]
+            if (indexImage > imageList.size - 1) indexImage = 0
+            imageId = imageList[indexImage]
             imageView2.setImageResource(imageId)
-          //  Log.d("MyLog","indexImage $indexImage")
+            //  Log.d("MyLog","indexImage $indexImage")
+        }
+        bDone.setOnClickListener {
+            val plant = Plant(imageId, edTitle.text.toString(), edDesc.text.toString())
+            val editIntent = Intent().apply {
+                putExtra("plant", plant)
+
+            }
+            setResult(RESULT_OK, editIntent)
+            finish()
         }
     }
 }
